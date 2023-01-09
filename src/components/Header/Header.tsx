da453a97075b989d44abd0fd352f490f4e9c2a85
sso214 <sojam214@gmail.com>
2023-01-09 17:33:08 +0900

    setting : React, TS, Rollup setup

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { box1, box2 } from "./styles";
+
+const Header = () => {
+  return (
+    <div>
+      <div css={box1}>
+        Header
+      </div>
+      <div css={box2}>
+        UI Component Test
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+export const box1 = css`
+  width: 100%;
+  height: 100px;
+  
+  color: #fff;
+  background: red;
+  text-align: center;
+`;
+
+export const box2 = css`
+  width: 100%;
+  height: 50px;
+
+  color: #fff;
+  background: blue;
+  text-align: center;
+`;
